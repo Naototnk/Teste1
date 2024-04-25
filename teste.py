5c06d279b113df5d9c5f7ba87bce26f1131dd963
naototnk <naototanaka123@hotmail.com>
2024-04-25 13:26:04 +0100

    Printado

--- teste.py
+++ teste.py
@@ -1,2 +1,20 @@
+
+#Git Add = Adiciona arquivo para ser commitado.
+
+#Git Commit = Registro de Alteração.
+
+#Git branch main = Criar uma Branch / ou uma (Cópia)
+
+#Git Remote add origin = Apontando que vou subir a Branch.
+
+#Git Push = Para subir as alterações feita na Branch.
+
+#Git Pull = Puxa a o que foi alterado.
+
+#Git Merge = Junta da cópia da branch, para a Main.
+
+print ("Todos vocês são gays")
+
+
 print("oi")
-print("Naotin")
+print("hi")
